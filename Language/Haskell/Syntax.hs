acbdfe1c92892e62d0fd090ed47ab60b51e6fff5
Don Stewart <dons@galois.com>
2010-11-07 23:09:13 +0800

    Remove -fglasgow-exts warning
    Ignore-this: 8a9bc9a1c8cd37fefbf7e23599f5503e
    
    darcs-hash:20101107150913-cba2c-47c83df829c9f9303b2801967802b71da9a7ec18.gz

--- Language/Haskell/Syntax.hs
+++ Language/Haskell/Syntax.hs
@@ -1,4 +1,5 @@
-{-# OPTIONS_GHC -fglasgow-exts -cpp #-}
+{-# LANGUAGE CPP                #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Language.Haskell.Syntax
